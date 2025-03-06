44c8434763cfdf59582a1a26bf17d1290f68e0c5
Nada Feteiha <nada.feteiha@gmail.com>
2025-03-06 07:48:06 -0800

    add the component

---
+++ src/components/Nav.jsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+
+export default function Nav(props) {
+    return (
+        <div className="nav">
+            <Link to="/">
+                <div>Crypto Prices</div>
+            </Link>
+
+            <Link to="/price">
+                <div>price</div>
+            </Link>
+
+            <Link to="/currencies">
+                <div>Currencies</div>
+            </Link>
+
+        </div>
+    );
+}
